efdc9f4cb752e66e48ba8460f004c28d2069f610
Tristan Matthias <hello@tristanmatthias.com>
2018-10-18 23:22:05 +1100

    refactor: moved to provider format

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,56 @@
-import fs from 'fs';
 import {Origami} from 'origami-core-lib';
 import Server from 'origami-core-server';
-import path from 'path';
-import {promisify} from 'util';
 import Media from './models/media';
-const {mkdir: mkdirAsync} = require('mkdir-recursive');
-
-const writeFile = promisify(fs.writeFile);
-const stat = promisify(fs.stat);
-const mkdir = promisify(mkdirAsync);
+import * as ProviderFilesystem from './providers/filesystem';
 
 
-export interface PluginOptions {
-    location: string;
+export type ProviderTypes = 's3' | 'filesystem';
+export type PluginOptionsBase = {
+    provider: ProviderTypes;
+};
+
+export interface PluginOptionsFileSystem extends PluginOptionsBase {
+    provider: 'filesystem';
+    location?: string;
+}
+export interface PluginOptionsS3 extends PluginOptionsBase {
+    provider: 's3';
+    bucket: string;
+    key: string;
 }
 
-interface req extends Origami.Server.Request {
-    files?: {
-        [key: string]: {
-            name: string;
-            mimetype: string;
-            provider: string;
-            data: Buffer;
-        }
-    };
+export type PluginOptions = PluginOptionsFileSystem | PluginOptionsS3;
+
+export interface Provider<T> {
+    initialize: (options: T) => any;
+    handlerCreate: (options: T) => Origami.Server.RequestHandler;
+    handlerGet: (options: T) => Origami.Server.RequestHandler;
 }
+
 
 module.exports = async(server: Server, options: PluginOptions) => {
 
-    const location = path.resolve(options.location);
-    try {
-        await stat(location);
-    } catch (e) {
-        mkdir(location);
+    let provider: Provider<PluginOptions>;
+    switch (options.provider) {
+        // case 's3':
+        //     provider = ProvidersFilesystem;
+        //     break;
+
+        default:
+        case 'filesystem':
+            provider = ProviderFilesystem;
+            break;
     }
+
+    await provider.initialize(options);
+
 
     server.resource('media', {
         model: Media,
-        auth: {
-            get: false
-        },
+        auth: {get: false},
         controllers: {
-            create: async(req: req, res, next) => {
-                if (!req.files) return next(new Error('No files uploaded'));
-
-                const [, file] = Object.entries(req.files)[0];
-
-                const m = server.store.model('media');
-
-                try {
-                    const data = await m.create({
-                        name: file.name,
-                        type: file.mimetype,
-                        provider: file.provider,
-                        author: req.jwt.data.userId
-                    }) as {id: string};
-
-                    const fp = path.resolve(location, data.id);
-                    writeFile(fp, file.data);
-                    res.data = data;
-
-                    next();
-                } catch (e) {
-                    next(e);
-                }
-            },
-            get: async(req, res, next) => {
-                const m = server.store.model('media') as Origami.Store.Model;
-                interface MediaResource {
-                    id: string;
-                    type: string;
-                }
-                const file = await m.find({id: req.params.mediaId}) as MediaResource;
-                if (!file) {
-                    res.responseCode = 'resource.errors.notFound';
-                    return next();
-                }
-                res.header('content-type', file.type);
-                res.sendFile(path.resolve(location, file.id));
-            }
+            create: provider.handlerCreate(options),
+            get: provider.handlerGet(options)
         }
     });
 };
---
+++ src/providers/filesystem.ts
@@ -0,0 +1,87 @@
+import fs from 'fs';
+import path from 'path';
+import {promisify} from 'util';
+import {PluginOptionsFileSystem} from '../';
+import {Origami} from 'origami-core-lib';
+
+const {mkdir: mkdirAsync} = require('mkdir-recursive');
+const writeFile = promisify(fs.writeFile);
+const stat = promisify(fs.stat);
+const mkdir = promisify(mkdirAsync);
+
+let location: string;
+
+export const initialize = async (options: PluginOptionsFileSystem) => {
+    location = path.resolve(process.cwd(), options.location || 'media');
+    console.log('got to create handler', location);
+
+    try {
+        await stat(location);
+    } catch (e) {
+        mkdir(location);
+    }
+};
+
+
+export interface req extends Origami.Server.Request {
+    files?: {
+        [key: string]: {
+            name: string;
+            mimetype: string;
+            provider: string;
+            data: Buffer;
+        }
+    };
+}
+
+
+export const handlerCreate = (
+    options: PluginOptionsFileSystem
+): Origami.Server.RequestHandler =>
+
+    async (req: req, res, next) => {
+        console.log('got to create handler', location);
+
+        if (!req.files) return next(new Error('No files uploaded'));
+
+        const [, file] = Object.entries(req.files)[0];
+
+        const m = res.app.get('store').model('media');
+
+        try {
+            const data = await m.create({
+                name: file.name,
+                type: file.mimetype,
+                provider: file.provider,
+                author: req.jwt.data.userId
+            }) as { id: string };
+
+            const fp = path.resolve(location, data.id);
+            writeFile(fp, file.data);
+            res.data = data;
+
+            next();
+        } catch (e) {
+            next(e);
+        }
+    };
+
+
+export const handlerGet = (
+    options: PluginOptionsFileSystem
+): Origami.Server.RequestHandler =>
+
+    async (req, res, next) => {
+        const m = res.app.get('store').model('media');
+        interface MediaResource {
+            id: string;
+            type: string;
+        }
+        const file = await m.find({id: req.params.mediaId}) as MediaResource;
+        if (!file) {
+            res.responseCode = 'resource.errors.notFound';
+            return next();
+        }
+        res.header('content-type', file.type);
+        res.sendFile(path.resolve(location, file.id));
+    };
